3b68d752250ad6369eb0ccb84b1bcf5ef03c50a1
magicianred <magicianred@gmail.com>
2020-12-05 22:23:19 +0100

    implemented pagination in PostsRepository

--- Magicianred.LearnByDoing.MyBlog.DAL/Repositories/PostsRepository.cs
+++ Magicianred.LearnByDoing.MyBlog.DAL/Repositories/PostsRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Magicianred.LearnByDoing.MyBlog.Domain.Interfaces.Repositories;
 using Magicianred.LearnByDoing.MyBlog.Domain.Models;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,13 +13,15 @@ namespace Magicianred.LearnByDoing.MyBlog.DAL.Repositories
     public class PostsRepository : IPostsRepository
     {
         private readonly IDatabaseConnectionFactory _connectionFactory;
+        private readonly IConfiguration _configuration;
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="configuration"></param>
-        public PostsRepository(IDatabaseConnectionFactory connectionFactory)
+        public PostsRepository(IDatabaseConnectionFactory connectionFactory, IConfiguration configuration)
         {
+            this._configuration = configuration;
             this._connectionFactory = connectionFactory;
         }
 
@@ -32,6 +35,34 @@ namespace Magicianred.LearnByDoing.MyBlog.DAL.Repositories
             using (var connection = _connectionFactory.GetConnection())
             {
                 posts = connection.Query<Post>("SELECT Id, Title, Text, Author FROM Posts ORDER BY CreateDate DESC");
+            }
+            return posts;
+        }
+
+        /// <summary>
+        /// Retrieve all Posts items paginated
+        /// </summary>
+        /// <param name="page">current page</param>
+        /// <param name="pageSize">items for page</param>
+        /// <returns>list of paginated post</returns>
+        public IEnumerable<Post> GetPaginatedAll(int page, int pageSize)
+        {
+            IEnumerable<Post> posts = null;
+            using (var connection = _connectionFactory.GetConnection())
+            {
+                var databaseType = _configuration.GetSection("DatabaseType").Value;
+                if (!string.IsNullOrWhiteSpace(databaseType) && databaseType.ToLower().Trim() == "mysql")
+                {
+                    posts = connection.Query<Post>(
+                        "SELECT Id, Title, Text, Author FROM Posts ORDER BY CreateDate DESC LIMIT @offset, @pageSize ",
+                        new { offset = ((page - 1) * pageSize), pageSize = pageSize });
+                }
+                else // if (!string.IsNullOrWhiteSpace(databaseType) && databaseType.ToLower().Trim() == "mssql")
+                {
+                    posts = connection.Query<Post>(
+                            "SELECT Id, Title, Text, Author FROM Posts ORDER BY CreateDate DESC OFFSET @offset ROWS FETCH NEXT @PageSize ROWS ONLY",
+                            new { offset = ((page - 1) * pageSize), pageSize = pageSize });
+                }
             }
             return posts;
         }
